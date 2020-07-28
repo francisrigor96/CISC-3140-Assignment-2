6fc5db166c65f24a47d31e32b0dd29bd5e65648a
francisrigor96 <42127741+francisrigor96@users.noreply.github.com>
2020-07-28 18:51:48 -0400

    Add files via upload
    Added java project to test subversion

---
+++ Assign2Subversion/ProjectFiles/SubversionTest/src/subversiontest/SubversionTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subversiontest;
+
+/**
+ *
+ * @author Francis
+ */
+public class SubversionTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello world! I'm adding some more text for Subversion!");
+    }
+    
+}
